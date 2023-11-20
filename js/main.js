01f5df7ae2f96ac1bd52378d0c1d90a32f1152b4
chiara.ruggi <chiara.ruggi1998@gmail.com>
2023-11-20 10:29:53 +0100

    remove list item

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
 const { createApp } = Vue;
 
 const opzioni = {
-    data: function () {
-        return {
-           
-        }
-        
+  data: function () {
+    return {
+      list: [
+        {
+            text: "fare la spesa",
+            check: false,
+        },
+        {
+            text: "fare la lavatrice",
+            check: false,
+        },
+        {
+            text: "rifare il letto",
+            check: false,
+        },
+      ],
+    };
+  },
+
+  methods: {
+    removeItem(i){
+        this.list.splice(i,1);
     },
-
-    methods: {
-
-    }, 
+  },
 };
 
-createApp(opzioni).mount('#app')
+createApp(opzioni).mount("#app");
